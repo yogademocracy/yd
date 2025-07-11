2791af7f2bfefc6d65100754513285c541af4056
Ivan Reznichenko <ivan.reznichenko@discoverember.com>
2025-07-11 16:15:32 +0300

    YD-19:ADD: updated search

--- cartridges/app_yd_core/cartridge/client/default/js/main.js
+++ cartridges/app_yd_core/cartridge/client/default/js/main.js
@@ -10,7 +10,7 @@ import cookie from 'base/components/cookie';
 import consentTracking from 'base/components/consentTracking';
 import footer from 'base/components/footer';
 import collapsibleItem from 'base/components/collapsibleItem';
-import search from './components/search';
+import search from 'base/components/search';
 import clientSideValidation from 'base/components/clientSideValidation';
 import countrySelector from 'base/components/countrySelector';
 import toolTip from 'base/components/toolTip';
