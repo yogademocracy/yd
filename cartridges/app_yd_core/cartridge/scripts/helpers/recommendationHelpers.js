8d8fcde4a7d18bc307734ab8b015e84833288696
Oleksandr Khalevskyi <Oleksandr.Khalevskyi@discoverember.com>
2025-07-28 16:36:07 +0300

    YD-337:ADD: BE PDP recommendations

---
+++ cartridges/app_yd_core/cartridge/scripts/helpers/recommendationHelpers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Gets recommendations for given product Id
+ * 
+ * @param {string} pid - product ID
+ * @returns {Array<Object>} Array of recommendation objects
+ */
+function getRecommendationsForProduct(pid) {
+    var ProductMgr = require('dw/catalog/ProductMgr');
+
+    var collections = require('*/cartridge/scripts/util/collections');
+
+    var recommendations = [];
+    var product = ProductMgr.getProduct(pid);
+
+    if (!product || empty(product.recommendations)) {
+        return recommendations;
+    }
+
+    collections.forEach(product.recommendations, (recommendation) => {
+        recommendations.push({
+            productID: recommendation.recommendedItemID,
+        });
+    });
+
+    return recommendations;
+};
+
+module.exports = {
+    getRecommendationsForProduct: getRecommendationsForProduct
+};
