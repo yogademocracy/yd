79b9d0e8341fbf94e5ef7b1b35a3f139ecc5d127
Borsch Serhiy <serhii.borshch@discoverember.com>
2025-07-31 00:59:21 +0300

    YD-315:FIX: cover cases when product attributes contain " symbol.

---
+++ cartridges/app_yd_core/cartridge/client/default/js/product/base.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var base = require('base/product/base');
+
+/**
+ * Process attribute values associated with an attribute that does not have image swatches
+ *
+ * @param {Object} attr - Attribute
+ * @param {string} attr.id - Attribute ID
+ * @param {Object[]} attr.values - Array of attribute value objects
+ * @param {string} attr.values.value - Attribute coded value
+ * @param {string} attr.values.url - URL to de/select an attribute value of the product
+ * @param {boolean} attr.values.isSelectable - Flag as to whether an attribute value can be
+ *     selected.  If there is no variant that corresponds to a specific combination of attribute
+ *     values, an attribute may be disabled in the Product Detail Page
+ * @param {jQuery} $productContainer - DOM container for a given product
+ */
+base.processNonSwatchValues = function processNonSwatchValues(attr, $productContainer) {
+    var $attr = '[data-attr="' + attr.id + '"]';
+    var $defaultOption = $productContainer.find($attr + ' .select-' + attr.id + ' option:first');
+    $defaultOption.attr('value', attr.resetUrl);
+
+    attr.values.forEach(function (attrValue) {
+        var $attrValue = $productContainer
+            .find($attr + ' [data-attr-value="' + CSS.escape(attrValue.value) + '"]');
+        $attrValue.attr('value', attrValue.url)
+            .removeAttr('disabled');
+
+        if (!attrValue.selectable) {
+            $attrValue.attr('disabled', true);
+        }
+    });
+};
+
+module.exports = base;
