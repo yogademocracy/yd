2af629bd82d6b756a1e76d9c4ad676c1fd5a04d1
Borsch Serhiy <serhii.borshch@discoverember.com>
2025-07-31 15:48:31 +0300

    YD-358:FIX: refactor.

--- cartridges/app_yd_core/cartridge/scripts/helpers/productHelpers.js
+++ cartridges/app_yd_core/cartridge/scripts/helpers/productHelpers.js
@@ -2,6 +2,28 @@
 
 var base = module.superModule;
 
+var baseGetAllBreadcrumbs = base.getAllBreadcrumbs;
+
+/**
+ * Creates the breadcrumbs object
+ * @param {string} cgid - category ID from navigation and search
+ * @param {string} pid - product ID
+ * @param {Array} breadcrumbs - array of breadcrumbs object
+ * @returns {Array} an array of breadcrumb objects
+ */
+function getAllBreadcrumbs(cgid, pid, breadcrumbs) {
+    var Resource = require('dw/web/Resource');
+    var URLUtils = require('dw/web/URLUtils');
+    var allBreadcrumbs = baseGetAllBreadcrumbs(cgid, pid, breadcrumbs);
+   
+    allBreadcrumbs.push({
+        htmlValue: Resource.msg('global.home', 'common', null),
+        url: URLUtils.url('Home-Show')
+    });
+    
+    return allBreadcrumbs;
+}
+
 /**
  * Renders the Product Details Page
  * @param {Object} querystring - query string parameters
@@ -9,23 +31,16 @@ var base = module.superModule;
  * @param {Object} usePageDesignerTemplates - wether to use the page designer version of the product detail templates, defaults to false
  * @returns {Object} contain information needed to render the product page
  */
-function showProductPage(querystring, reqPageMetaData) {
+base.showProductPage = function (querystring, reqPageMetaData) {
     var URLUtils = require('dw/web/URLUtils');
     var ProductFactory = require('*/cartridge/scripts/factories/product');
     var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
-    var Resource = require('dw/web/Resource');
 
     var params = querystring;
     var product = ProductFactory.get(params);
     var addToCartUrl = URLUtils.url('Cart-AddProduct');
     var canonicalUrl = URLUtils.url('Product-Show', 'pid', product.id);
-    var homePageBreadcrumb = {
-        htmlValue: Resource.msg('global.home', 'common', null),
-        url: URLUtils.url('Home-Show')
-    };
-    var breadcrumbs = base.getAllBreadcrumbs(null, product.id, []);
-    breadcrumbs.push(homePageBreadcrumb);
-    breadcrumbs.reverse();
+    var breadcrumbs = getAllBreadcrumbs(null, product.id, []).reverse();
 
     var template = 'product/productDetails';
 
@@ -50,8 +65,7 @@ function showProductPage(querystring, reqPageMetaData) {
         canonicalUrl: canonicalUrl,
         schemaData: schemaData
     };
-}
+};
 
-base.showProductPage = showProductPage;
-
+base.getAllBreadcrumbs = getAllBreadcrumbs;
 module.exports = base;
