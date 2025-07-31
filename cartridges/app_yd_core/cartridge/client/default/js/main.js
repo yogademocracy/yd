09720142caec8d9c58ad0d005891ba00a3a23c59
Borsch Serhiy <serhii.borshch@discoverember.com>
2025-07-31 15:08:57 +0300

    YD-315:FIX: key client scripts replaced in core cartridge.

--- cartridges/app_yd_core/cartridge/client/default/js/main.js
+++ cartridges/app_yd_core/cartridge/client/default/js/main.js
@@ -15,8 +15,8 @@ import search from 'base/components/search';
 import clientSideValidation from 'base/components/clientSideValidation';
 import countrySelector from 'base/components/countrySelector';
 import toolTip from 'base/components/toolTip';
-import miniCart from 'base/components/miniCart';
 import customModal from './components/customModal';
+import miniCart from './components/miniCart';
 
 $(document).ready(() => {
     processInclude(menu);
@@ -29,6 +29,7 @@ $(document).ready(() => {
     processInclude(clientSideValidation);
     processInclude(countrySelector);
     processInclude(toolTip);
+    processInclude(miniCart);
     processInclude(customModal);
 });
 
