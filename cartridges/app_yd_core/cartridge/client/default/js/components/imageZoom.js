f23a1cc05855de493fa2c5de93eb50d01bd58b3e
Oleksandr Khalevskyi <Oleksandr.Khalevskyi@discoverember.com>
2025-08-11 17:52:12 +0300

    YD-346:FIX: zoom reinit on PDP

--- cartridges/app_yd_core/cartridge/client/default/js/components/imageZoom.js
+++ cartridges/app_yd_core/cartridge/client/default/js/components/imageZoom.js
@@ -1,12 +1,23 @@
 import Drift from 'drift-zoom';
 
-$('.js-image-zoom').each(function () {
-    const settings = {
-        inlinePane: true,
-        injectBaseStyles: true,
-        zoomFactor: 2,
-        sourceAttribute: 'src'
-    };
+/**
+ * Init zoom on PDP images
+ */
+function initZoom() {
+    $('.js-image-zoom').each(function () {
+        const settings = {
+            inlinePane: true,
+            injectBaseStyles: true,
+            zoomFactor: 2,
+            sourceAttribute: 'src'
+        };
 
-    new Drift(this, settings);
+        new Drift(this, settings);
+    });
+}
+
+$('body').on('product:afterAttributeSelect', function () {
+    initZoom();
 });
+
+initZoom();
