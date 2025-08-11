9d3621a7fba011a2eacf77f0bb3b01d8cdb6b2e4
OleksandrKhalevskyi <oleksandr.khalevskyi@discoverember.com>
2025-08-11 11:36:48 +0300

    YD-367:ADD: Redeem Gift Certificate on checkout (#56)
    * YD-367:ADD: Redeem Gift Certificate on checkout
    
    * YD-367:ADD: Remove ESlint rule in file
    
    * YD-367:ADD: meta and fixed client scripts
    
    * YD-367:ADD: fixed cleave and redirect to order confirmation
    
    * YD-367:ADD: removd eslint rule
    
    * YD-367:ADD: comment to checkout file; esling globl rules

---
+++ cartridges/app_yd_core/cartridge/client/default/js/checkout/billing.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var cybersource = require('cybersource/checkout/billing');
+var cleave = require('cybersource/checkout/cleave');
+
+/**
+ * Validate and update payment instrument form fields
+ * @param {Object} order - the order model
+ */
+cybersource.methods.validateAndUpdateBillingPaymentInstrument = function (order) {
+    var billing = order.billing;
+    if (!billing.payment || !billing.payment.selectedPaymentInstruments
+        || billing.payment.selectedPaymentInstruments.length <= 0) return;
+
+    var form = $('form[name=dwfrm_billing]');
+    if (!form) return;
+
+    var instrument = billing.payment.selectedPaymentInstruments[0];
+    $('select[name$=expirationMonth]', form).val(instrument.expirationMonth);
+    $('select[name$=expirationYear]', form).val(instrument.expirationYear);
+    // Force security code and card number clear
+    $('input[name$=securityCode]', form).val('');
+
+    if ($('input[name$=cardNumber]').length) {
+        $('input[name$=cardNumber]').data('cleave').setRawValue('');
+    }
+};
+
+cybersource.handleCreditCardNumber = function () {
+    if ($('.cardNumber').length && $('#cardType').length) {
+        cleave.handleCreditCardNumber('.cardNumber', '#cardType');
+    }
+};
+
+module.exports = cybersource;
