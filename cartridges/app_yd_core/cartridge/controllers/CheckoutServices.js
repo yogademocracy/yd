326764be67d20c7685d99ff61197c0dac6a5a83f
Borsch-Serhiy <66720711+Borsch-Serhiy@users.noreply.github.com>
2025-08-13 11:58:33 +0300

    YD-000:ADD: set order payment status. (#75)
    * YD-000:ADD: set order payment status.
    
    * YD-000:ADD: improvements.
    
    * YD-000:ADD: improvements, used constant instead of hardcoded value.
    
    * YD-000:ADD: improvements, used setter instead of direct set to property.

---
+++ cartridges/app_yd_core/cartridge/controllers/CheckoutServices.js
@@ -0,0 +1,29 @@
+var server = require('server');
+server.extend(module.superModule);
+
+server.append('PlaceOrder', function (req, res, next) {
+    var OrderMgr = require('dw/order/OrderMgr');
+    var Transaction = require('dw/system/Transaction');
+    var URLUtils = require('dw/web/URLUtils');
+    var Site = require('dw/system/Site');
+
+    if (Site.current.getCustomPreferenceValue('Cybersource_CardTransactionType').value.toLowerCase() === 'sale' && !session.privacy.AuthorizeErrors) {
+        Transaction.wrap(function () {
+            var orderID = res.getViewData().orderID;
+            var order = OrderMgr.getOrder(orderID);
+
+            order.setPaymentStatus(order.PAYMENT_STATUS_PAID);
+        });
+
+        res.json({
+            error: false,
+            ID: res.getViewData().orderID,
+            token: res.getViewData().token,
+            continueUrl: URLUtils.url('COPlaceOrder-SubmitOrderConformation').toString()
+        });
+    }
+
+    return next();
+});
+
+module.exports = server.exports();
